efb7999e3f2b793f7ebf89ac3d3d5a099f1d782e
Dmitry Kulakov <atomeistee@gmail.com>
2023-02-18 10:15:14 +0300

    update comparing graph

--- src/pages/compare.py
+++ src/pages/compare.py
@@ -4,9 +4,12 @@ import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 
-@callback(Output('bar-chart', 'children'),Input('first', 'value'), Input('second', 'value'))
+@callback(Output('bar-chart', 'figure'),Input('first', 'value'), Input('second', 'value'))
 def display_output(first, second):
-    if first == None or second == None: return ''
+    if first == None or second == None: return go.Figure(data=[
+go.Bar(name=f'Регион 1', y=["Признак 1", "Признак 2", 'Признак 3'], x=[20, 14, 23], orientation='h'),
+go.Bar(name=f'Регион 2', y=["Признак 1", "Признак 2", 'Признак 3'], x=[12, 18, 29], orientation='h')
+])
     print(f'Регион {first}')
     print(f'Регион {second}')
     fig = go.Figure(data=[
@@ -14,7 +17,8 @@ def display_output(first, second):
     go.Bar(name=f'Регион {second}', y=["Признак 1", "Признак 2", 'Признак 3'], x=[12, 18, 29], orientation='h')
 ])
     fig.update_layout(barmode='group')
-    return dcc.Graph(id='bar-chart', figure=fig)
+    return fig
+
 
 compare = html.Div(
     [
@@ -46,8 +50,8 @@ compare = html.Div(
             style={"display": "flex", "gap": "20px"}),
             html.Div([card(22,223,'Бюджет', True),
             card(223,150,'Волонтеры'),
-            card(2222,13111,'СМИ расходы', True)], style={"display": "flex", "gap": "20px", "margin": "1rem" }),
+            card(2222,13111,'СМИ расходы', True)], style={"display": "flex", "gap": "20px", "margin": "1rem 0" }),
             
-            html.Div(id='bar-chart')
+            dcc.Graph(id='bar-chart')
     ]
 )
