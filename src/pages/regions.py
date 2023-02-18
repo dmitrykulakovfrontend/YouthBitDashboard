a87b40ab30172e05c8fbd8b7de4cd527c514301d
Dmitry Kulakov <atomeistee@gmail.com>
2023-02-18 17:54:36 +0300

    fix deploy issue

--- src/pages/regions.py
+++ src/pages/regions.py
@@ -5,13 +5,13 @@ import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 
-df = pd.read_csv('out.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/dmitrykulakovfrontend/YouthBitDashboard/main/out.csv')
 
 
 @callback(Output("charts", "style"),  Input("region", "value"), Input("type", "value"))
 def toggle_menu(val1, val2):
     if val1 == None or val2 == None: return {"display": "none"}
-    return {"display": "flex"}
+    return {"display": "block", "position": "relative", "right": "50px"}
 
 @callback(Output("cards", "children"),Output('histogram', "figure"),Output('histogram2', "figure"), Input("region", "value"), Input("type", "value"))
 def display_region_info(region_name, type):
@@ -45,11 +45,13 @@ def display_region_info(region_name, type):
     color = "green" if population_involvement == "Высокий" else "red" if population_involvement == "Низкий" else "orange"
 
     fig = go.Figure(data=[
-            go.Bar( y=['Бюджет МиММ', 'Бюджет ПВ', 'Бюджет ВД', 'Бюджет ПиП', 'Бюджет других отделений'], x=[x1,x2,x3,x4,x5], orientation='h')
+            go.Bar( y=['Бюджет Развития международного <br>и межрегионального молодeжного<br> сотрудничества', 'Бюджет Патриотического <br>воспитание молодeжи', 'Бюджет Волонтёрской<br>деятельности', 'Бюджет Содействия<br> в подготовке и переподготовке <br>специалистов', 'Бюджет других<br> отделений'], x=[x1,x2,x3,x4,x5], orientation='h')
     ])
     fig2 = go.Figure(data=[
-            go.Bar( y=['Численность МиММ', 'Численность ПВ', 'Численность ВД', 'Численность ПиП', 'Численность других расходов'], x=[y1,y2,y3,y4,y5], orientation='h')
+            go.Bar( y=['Численность Развития<br> международного и межрегионального<br> молодeжного сотрудничества', 'Численность Патриотического <br>воспитание молодeжи', 'Численность Волонтёрской<br> деятельности', 'Численность Содействия<br> в подготовке и переподготовке<br> специалистов', 'Численность других<br> расходов'], x=[y1,y2,y3,y4,y5], orientation='h')
     ])
+    fig.update_layout( bargap=0.30)
+    fig2.update_layout( bargap=1)
 
     return [card(budget,budget_per_person,'Бюджет на человека','Бюджет', True),
             card(youth_involvement,population_involvement,'Уровень вовлеченности населения','Количество вовлеченной молодежи', color=color)], fig, fig2
@@ -60,7 +62,7 @@ regions = html.Div(
     [
         html.H1('Информация о регионах',
                 style={'color': "#897AD6"}),
-        html.Label('Выберите регион и тип данных для подробной информации', style={"text-align": "center", "width": "100%", "font-weight": "bold", "margin": "1rem 0", "font-size": "1.5rem"}),
+        html.Label('Выберите регион и тип данных для подробной информации', style={"text-align": "left", "width": "100%", "font-weight": "bold", "margin": "1rem 0", "font-size": "1.5rem"}),
         html.Div([
             dcc.Dropdown(
                 options=[
@@ -80,8 +82,8 @@ regions = html.Div(
                 placeholder="Выберите данные"
             )
         ],
-            style={"display": "flex", "gap": "20px", "justify-content": "center"}),
-            html.Div(id="cards", style={"display": "flex", "gap": "20px", "margin": "1rem 0", "flex-wrap": "wrap", "justify-content": "center" }),
+            style={"display": "flex", "gap": "20px", "justify-content": "flex-start"}),
+            html.Div(id="cards", style={"display": "flex", "gap": "20px", "margin": "1rem 0", "flex-wrap": "wrap", "justify-content": "flex-start" }),
             html.Div([dcc.Graph(id="histogram",),
             dcc.Graph(id="histogram2",)], style={"display": "flex"}, id="charts")
             
